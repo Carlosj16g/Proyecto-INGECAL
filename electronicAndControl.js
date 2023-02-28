ce3b8fe375b70b817ddb915d73963b01579a9ffc
r4vc31 <r4vc31@gmail.com>
2023-02-28 13:02:12 -0500

    completada lógica y visual para cálculo de resistencia (serie)

--- electronicAndControl.js
+++ electronicAndControl.js
@@ -1,29 +1,37 @@
 function iniciarMenu() {
 
     let menu = document.getElementById("menu");
-    menu.addEventListener("click", function() {
+    menu.addEventListener("change", function () {
         let optionSelected = menu.options[menu.selectedIndex].getAttribute("value");
         if (optionSelected === "1") {
             document.getElementById("option-menu-1").style.display = "flex";
-            let resistances = [];
-            for (let resistance of document.getElementsByClassName("data-1")){
-                if (resistance.value === undefined) {
-                    alert("Complete los campos");
-                    resistances = [];
-                    break;
-                }else {
-                    resistances.push(resistance.value)
+            document.getElementById("equivalent-resistance").addEventListener("click", function () {
+                let resistances = [];
+                for (let resistance of document.getElementsByClassName("data-1")) {
+                    if (resistance.value === undefined) {
+                        alert("Complete los campos");
+                        resistances = [];
+                        break;
+                    } else {
+                        console.log(resistance.value);
+                        resistances.push(parseFloat(resistance.value));
+                    }
                 }
-            }
-            
-        }
-        else{
+                let result = getEquivalentResistance(resistances);
+                console.log(result);
+                document.getElementById("show-result").value = result;
+            })
+            document.getElementById("add-fields").addEventListener("click", function () {
+                let totalFields = document.getElementsByClassName("data-1").length;
+                document.getElementById("fields").insertAdjacentHTML("beforeend",`<label for="">Resistencia ${totalFields + 1}: <input class="data-1" type="number"></label>`);
+            })
+        } else {
             document.getElementById("option-menu-1").style.display = "none";
         }
     })
-    
 
-    
+
+
 }
 
 function getEquivalentResistance(resistances) {
