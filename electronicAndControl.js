818a29a065c1b0e39f99382ae99dd8bcbea7b3bc
r4vc31 <r4vc31@gmail.com>
2023-03-01 16:55:00 -0500

    working in inductor and capacitor equivalent

--- electronicAndControl.js
+++ electronicAndControl.js
@@ -1,8 +1,29 @@
 function iniciarMenu() {
 
+    for (let resistance of document.getElementsByClassName("data-1")) {
+        resistance.addEventListener("click", function () {
+            document.getElementById("show-result").value = "Waiting Data ...";
+            document.getElementById("resistencia-serie").style.display = "none";
+            document.getElementById("resistencia-paralelo").style.display = "none";
+        })
+    }
+
     document.getElementById("add-fields").addEventListener("click", function () {
         let totalFields = document.getElementsByClassName("data-1").length;
         document.getElementById("fields").insertAdjacentHTML("beforeend", `<label for="">Resistencia ${totalFields + 1}: <input class="data-1" type="number"></label>`);
+        let count = 0;
+        for (let resistance of document.getElementsByClassName("data-1")) {
+            if (count >= totalFields - 1) {
+                resistance.addEventListener("click", function () {
+                    document.getElementById("show-result").value = "Waiting Data ...";
+                    document.getElementById("resistencia-serie").style.display = "none";
+                    document.getElementById("resistencia-paralelo").style.display = "none";
+                })
+            } else {
+                count += 1;
+                continue;
+            }
+        }
     })
 
     let menu = document.getElementById("menu");
@@ -33,11 +54,11 @@ function iniciarMenu() {
 
                     document.getElementById("resistencia-serie").addEventListener("click", function () {
                         let result = getEquivalentResistanceSerie(resistances);
-                        document.getElementById("show-result").value = result;
+                        document.getElementById("show-result").value = result.toFixed(3);
                     });
                     document.getElementById("resistencia-paralelo").addEventListener("click", function () {
                         let result = getEquivalentResistanceParalelo(resistances);
-                        document.getElementById("show-result").value = result;
+                        document.getElementById("show-result").value = result.toFixed(3);
                     });
                 } else {
                     alert("Complete los campos");
@@ -63,9 +84,25 @@ function getEquivalentResistanceSerie(resistances) {
 function getEquivalentResistanceParalelo(resistances) {
     let result = 0;
     resistances.map(resistance => {
-        result += 1/resistance
+        result += 1 / resistance
     })
-    return 1/result;
+    return 1 / result;
+}
+
+function getEquivalentCapacitorSerie(capacitors) {
+    return getEquivalentResistanceParalelo(capacitors);
+}
+
+function getEquivalentCapacitorParalelo(capacitors) {
+    return getEquivalentResistanceSerie(capacitors);
+}
+
+function getEquivalentInductorSerie(inductors) {
+    return getEquivalentResistanceSerie(inductors);
+}
+
+function getEquivalentInductorParalelo(inductors) {
+    return getEquivalentResistanceParalelo(inductors);
 }
 
 window.addEventListener("load", iniciarMenu);
