0d8a867249e41278b44972a0cf069013a807e661
r4vc31 <r4vc31@gmail.com>
2023-03-02 10:38:08 -0500

    Inductor Option Completed

--- electronicAndControl.js
+++ electronicAndControl.js
@@ -1,5 +1,11 @@
 function iniciarMenu() {
 
+    startOption1();
+    startOption2();
+}
+
+
+function startOption1() {
     for (let resistance of document.getElementsByClassName("data-1")) {
         resistance.addEventListener("click", function () {
             document.getElementById("show-result").value = "Waiting Data ...";
@@ -68,9 +74,77 @@ function iniciarMenu() {
             document.getElementById("option-menu-1").style.display = "none";
         }
     })
+}
 
+function startOption2() {
+    for (let inductor of document.getElementsByClassName("data-3")) {
+        inductor.addEventListener("click", function () {
+            document.getElementById("show-inductor-result").value = "Waiting Data ...";
+            document.getElementById("inductor-serie").style.display = "none";
+            document.getElementById("inductor-paralelo").style.display = "none";
+        })
+    }
 
+    document.getElementById("add-inductor-fields").addEventListener("click", function () {
+        let totalFields = document.getElementsByClassName("data-3").length;
+        document.getElementById("inductor-fields").insertAdjacentHTML("beforeend", `<label for="">Inductor ${totalFields + 1}: <input class="data-3" type="number"></label>`);
+        let count = 0;
+        for (let inductor of document.getElementsByClassName("data-3")) {
+            if (count >= totalFields - 1) {
+                inductor.addEventListener("click", function () {
+                    document.getElementById("show-inductor-result").value = "Waiting Data ...";
+                    document.getElementById("inductor-serie").style.display = "none";
+                    document.getElementById("inductor-paralelo").style.display = "none";
+                })
+            } else {
+                count += 1;
+                continue;
+            }
+        }
+    })
 
+    let menu = document.getElementById("menu");
+    menu.addEventListener("change", function () {
+        let optionSelected = menu.options[menu.selectedIndex].getAttribute("value");
+        if (optionSelected === "3") {
+            document.getElementById("option-menu-3").style.display = "flex";
+            document.getElementById("equivalent-inductor").addEventListener("click", function () {
+
+                document.getElementById("show-inductor-result").value = "Waiting Data ..."
+
+                let inductors = [];
+                for (let inductor of document.getElementsByClassName("data-3")) {
+                    if (inductor.value !== "") {
+                        inductors.push(parseFloat(inductor.value));
+                    } else {
+                        inductors = [];
+                        break;
+                    }
+                }
+                console.log("inductors.length", inductors.length);
+                if (inductors.length) {
+
+                    document.getElementById("show-inductor-result").value = "Structure?"
+
+                    document.getElementById("inductor-serie").style.display = "inline-block";
+                    document.getElementById("inductor-paralelo").style.display = "inline-block";
+
+                    document.getElementById("inductor-serie").addEventListener("click", function () {
+                        let result = getEquivalentInductorSerie(inductors);
+                        document.getElementById("show-inductor-result").value = result.toFixed(3);
+                    });
+                    document.getElementById("inductor-paralelo").addEventListener("click", function () {
+                        let result = getEquivalentInductorParalelo(inductors);
+                        document.getElementById("show-inductor-result").value = result.toFixed(3);
+                    });
+                } else {
+                    alert("Complete los campos");
+                }
+            })
+        } else {
+            document.getElementById("option-menu-3").style.display = "none";
+        }
+    })
 }
 
 function getEquivalentResistanceSerie(resistances) {
